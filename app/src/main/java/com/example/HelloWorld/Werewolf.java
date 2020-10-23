e91b6bcb1d79aa2150f5f74ae9c3da62076b8245
s1971119 <ma2rin_121624@icloud.com>
2020-10-23 17:37:13 +0900

    人狼の役職のイラストを追加しました。

--- app/src/main/java/com/example/HelloWorld/Werewolf.java
+++ app/src/main/java/com/example/HelloWorld/Werewolf.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,7 +12,6 @@ import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Werewolf extends AppCompatActivity {
     int peopleNum = 3; //人数
