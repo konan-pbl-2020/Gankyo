a6aa638a18cb0fd83ef968534b2ec03abafc061b
na-na-ko <h120620nanako@icloud.com>
2020-11-06 15:52:34 +0900

    ジャンケンのプログラムを変更しました。

--- app/src/main/java/com/example/HelloWorld/RPS.java
+++ app/src/main/java/com/example/HelloWorld/RPS.java
@@ -3,12 +3,19 @@ package com.example.HelloWorld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class RPS extends AppCompatActivity {
 
@@ -18,39 +25,89 @@ public class RPS extends AppCompatActivity {
         setContentView(R.layout.activity_r_p_s);
         final TextView text = findViewById(R.id.text);
         final TextView subtext = findViewById(R.id.subtext);
-        Button rock = (Button)findViewById(R.id.rock);
-        Button scissors = (Button)findViewById(R.id.scissors);
-        Button paper = (Button)findViewById(R.id.paper);
+        final ImageButton rockButton = (ImageButton)findViewById(R.id.rockButton);
+        final ImageButton scissorsButton = (ImageButton)findViewById(R.id.scissorsButton);
+        final ImageButton paperButton = (ImageButton)findViewById(R.id.paperButton);
+        final ImageView opponentImage = (ImageView) findViewById(R.id.opponent);
+        opponentImage.setImageResource(R.drawable.bot_hand_rock);
+        final Button ok = (Button) findViewById(R.id.okButton);
+        ok.setVisibility(INVISIBLE);
+        ok.setEnabled(false);
+        Button back = (Button)findViewById(R.id.back);
 
         //opponentHand 1  "rock"
         //opponentHand 2  "scissors"
         //opponentHand 3  "paper"
-        rock.setOnClickListener(new View.OnClickListener() {
+
+        rockButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                rockButton.setEnabled(false);
+                scissorsButton.setVisibility(INVISIBLE);
+                scissorsButton.setEnabled(false);
+                paperButton.setVisibility(INVISIBLE);
+                paperButton.setEnabled(false);
+                ok.setVisibility(VISIBLE);
+                ok.setEnabled(true);
                 int opponentHand = decideOpponentHand();
-                String opponentHandText = changeTextOpponentHand(opponentHand);
                 decideGame(1, opponentHand, subtext);
-                text.setText(opponentHandText);
+                text.setText("ぽん");
+                if(opponentHand == 1) opponentImage.setImageResource(R.drawable.bot_hand_rock);
+                else if(opponentHand == 2) opponentImage.setImageResource(R.drawable.bot_hand_scissors);
+                else opponentImage.setImageResource(R.drawable.bot_hand_paper);
             }
         });
-        scissors.setOnClickListener(new View.OnClickListener() {
+        scissorsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                rockButton.setVisibility(INVISIBLE);
+                rockButton.setEnabled(false);
+                scissorsButton.setEnabled(false);
+                paperButton.setVisibility(INVISIBLE);
+                paperButton.setEnabled(false);
+                ok.setVisibility(VISIBLE);
+                ok.setEnabled(true);
                 int opponentHand = decideOpponentHand();
-                String opponentHandText = changeTextOpponentHand(opponentHand);
                 decideGame(2, opponentHand, subtext);
-                text.setText(opponentHandText);
+                text.setText("ぽん");
+                if(opponentHand == 1) opponentImage.setImageResource(R.drawable.bot_hand_rock);
+                else if(opponentHand == 2) opponentImage.setImageResource(R.drawable.bot_hand_scissors);
+                else opponentImage.setImageResource(R.drawable.bot_hand_paper);
             }
         });
-        paper.setOnClickListener(new View.OnClickListener() {
+        paperButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                rockButton.setVisibility(INVISIBLE);
+                rockButton.setEnabled(false);
+                scissorsButton.setVisibility(INVISIBLE);
+                scissorsButton.setEnabled(false);
+                paperButton.setEnabled(false);
+                ok.setVisibility(VISIBLE);
+                ok.setEnabled(true);
                 int opponentHand = decideOpponentHand();
-                String opponentHandText = changeTextOpponentHand(opponentHand);
                 decideGame(3, opponentHand, subtext);
-                text.setText(opponentHandText);
+                text.setText("ぽん");
+                if(opponentHand == 1) opponentImage.setImageResource(R.drawable.bot_hand_rock);
+                else if(opponentHand == 2) opponentImage.setImageResource(R.drawable.bot_hand_scissors);
+                else opponentImage.setImageResource(R.drawable.bot_hand_paper);
             }
         });
 
-        Button back = (Button)findViewById(R.id.back);
+        ok.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ok.setVisibility(INVISIBLE);
+                ok.setEnabled(false);
+                rockButton.setVisibility(VISIBLE);
+                rockButton.setEnabled(true);
+                scissorsButton.setVisibility(VISIBLE);
+                scissorsButton.setEnabled(true);
+                paperButton.setVisibility(VISIBLE);
+                paperButton.setEnabled(true);
+                text.setText("最初はグー");
+                subtext.setText("じゃんけん");
+                opponentImage.setImageResource(R.drawable.bot_hand_rock);
+                subtext.setTextColor(Color.rgb(255, 0, 0));
+            }
+        });
+
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(RPS.this,MainActivity.class);
@@ -58,23 +115,27 @@ public class RPS extends AppCompatActivity {
             }
         });
     }
-    String changeTextOpponentHand(int hand) {
-        String handText = "";
-        if (hand == 1) handText = "グー";
-        else if (hand == 2) handText = "チョキ";
-        else if (hand == 3) handText = "パー";
-        return handText;
-    }
+
     int decideOpponentHand() {
         Random rnd = new Random();
         int hand = rnd.nextInt(3)+1;
         return hand;
     }
+
     void decideGame(int playerHand, int opponentHand, TextView subtext) {
         String decision;
-        if(playerHand == opponentHand) decision = "あいこ";
-        else if((playerHand == 3 && opponentHand == 1) || (playerHand+1 == opponentHand)) decision = "勝ち";
-        else decision = "負け";
+        if(playerHand == opponentHand){
+            decision = "あいこ";
+            subtext.setTextColor(Color.rgb(0, 0, 0));
+        }
+        else if((playerHand == 3 && opponentHand == 1) || (playerHand+1 == opponentHand)){
+            decision = "勝ち";
+            subtext.setTextColor(Color.rgb(255, 0, 0));
+        }
+        else{
+            decision = "負け";
+            subtext.setTextColor(Color.rgb(0, 0, 255));
+        }
         subtext.setText(decision);
     }
 }
