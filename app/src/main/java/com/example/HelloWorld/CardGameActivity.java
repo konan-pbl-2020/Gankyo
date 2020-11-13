5e235c7882b0b081419d78237696bad82cef24d2
YoshimuraYusuke <simusi903@ezweb.ne.jp>
2020-11-13 16:09:49 +0900

    ブロック崩しのメインアクティビティを追加しました。

---
+++ app/src/main/java/com/example/HelloWorld/CardGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.HelloWorld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_game);
+    }
+}
